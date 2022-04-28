6c471836a37e00da7964e031b6b41cafe9ee1faa
andreylazeev <lazeev1998@yandex.ru>
2022-04-28 11:07:06 +0700

    maybe fix ctrl z

--- src/listeners.ts
+++ src/listeners.ts
@@ -1,37 +1,37 @@
-import { app, getMatrix, Matrix, setActiveNavigationElement, setMatrix, updateMatrixState } from './index'
+import { app, getMatrix, Matrix, setActiveNavigationElement, setMatrix, updateMatrixState } from './index';
 const changesHistory: Array<Matrix> = [[]];
 
-function saveHistory () {
-    const nextMatrix = JSON.stringify(getMatrix());
+function saveHistory() {
+  const nextMatrix = JSON.stringify(getMatrix());
 
-    if (JSON.stringify(changesHistory[changesHistory.length - 1]) !== nextMatrix) {
-        changesHistory.push(JSON.parse(nextMatrix))
-    }
+  if (JSON.stringify(changesHistory[changesHistory.length - 1]) !== nextMatrix) {
+    changesHistory.push(JSON.parse(nextMatrix));
+  }
 }
 
 export function attachMouseListeners() {
-    app.renderer.view.addEventListener('click', updateMatrixState)
+  app.renderer.view.addEventListener('click', updateMatrixState);
 
-    app.renderer.view.addEventListener('mousedown', function (e) {
-        saveHistory()
-        app.renderer.view.addEventListener('mousemove', updateMatrixState)
-    });
+  app.renderer.view.addEventListener('mousedown', function (e) {
+    saveHistory();
+    app.renderer.view.addEventListener('mousemove', updateMatrixState);
+  });
 
-    app.renderer.view.addEventListener('mouseup', function (e) {
-        app.renderer.view.removeEventListener('mousemove', updateMatrixState)
-    });
+  app.renderer.view.addEventListener('mouseup', function (e) {
+    app.renderer.view.removeEventListener('mousemove', updateMatrixState);
+  });
 }
 
 export function attachKeyboardListeners() {
-    document.body.addEventListener('keypress', (e => {
-        setActiveNavigationElement(e);
+  document.body.addEventListener('keydown', (e) => {
+    setActiveNavigationElement(e);
+    console.log(e.ctrlKey);
 
-        if (e.key === 'z' && e.ctrlKey || e.key === 'z' && e.ctrlKey) {
-            if (changesHistory.length > 1) {
-                setMatrix(changesHistory[changesHistory.length - 1])
-                changesHistory.pop()
-            }
-        }
-    }))
+    if ((e.key === 'z' && e.ctrlKey) || (e.key === 'z' && e.ctrlKey)) {
+      if (changesHistory.length > 1) {
+        setMatrix(changesHistory[changesHistory.length - 1]);
+        changesHistory.pop();
+      }
+    }
+  });
 }
-
